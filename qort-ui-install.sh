7eec0d47efcff6a1b6dd9c85f3f2a11924563b7a
crowetic <5431064+crowetic@users.noreply.github.com>
2020-03-09 14:14:49 -0700

    updated all echoes and git repos, reworded things.

--- qort-ui-install.sh
+++ qort-ui-install.sh
@@ -23,15 +23,19 @@ function print_help()
   echo -e "Syntax: $0 [-i] [-b] [-p] >&2"
   echo -e "\t[-i]: install every repository needed, install dependencies, clone git repos, do yarn linking and building, and build and run final UI"
   echo -e "\t[-b]: update all git repos, stash, pull, yarn relink and install, and rebuild"
-  echo -e "\t[-p]: check for github updates with a pull"
+  echo -e "\t[-p]: not implemented yet"
   exit 1
 }
 
 # install every repository needed, install dependencies, clone git repos, do yarn linking and building, and build and run final UI
 function process_full_install()
 {
+  echo -e '---DOING APT UPDATE FIRST---'
+  
   sudo apt update
-  echo -e 'setting up yarn source and node soruce with possible node setup'
+  
+  echo -e '--SETTING UP YARN AND NODE APT SOURCES AND INSTALLING DEPENDENCIES FOR NODE ATTEMPT ONE---'
+  
   sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
   #curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -41,27 +45,43 @@ function process_full_install()
 #  echo -e 'installing node.js 12 sources'
 #  sudo echo "deb https://deb.nodesource.com/node_12.x eoan main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 #  sudo echo 'deb-src https://deb.nodesource.com/node_12.x eoan main' >> /etc/apt/sources.list.d/nodesource.list
-  echo -e 'INSTALLING NODEJS 12+ SOURCES'
+  
+  echo -e '---INSTALLING NODEJS 12+ SOURCES WITH TWO POSSIBLE OPTIONS IN CASE OF FAILURE OF ONE---'
+  
   sudo echo "deb https://deb.nodesource.com/node_12.x eoan main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   sudo echo 'deb-src https://deb.nodesource.com/node_12.x eoan main' >> /etc/apt/sources.list.d/nodesource.list
   sudo apt update
 #  sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+  
   echo -e '---INSTALLING DEPENDENCIES!---'
+  
   sudo apt update  
-  echo -e 'NODE.JS'
+  
+  echo -e '---INSTALLING NODE.JS---'
+  
   sudo apt -y install nodejs
-  echo -e 'YARN'
+  
+  echo -e '---INSTALLING YARN---'
+  
   sudo apt -y install yarn
-  echo -e 'NPM'
+  
+  echo -e '---INSTALLING NPM AND ZIP/UNZIP---'
+  
   sudo apt -y install npm
   sudo apt -y install zip
   sudo apt -y install unzip
-  echo -e 'GIT'
+  
+  echo -e '---INSTALLING GIT---'
+  
   sudo apt -y install git
+  
   echo -e '---CLONING ALL UI REPOSITORIES---'
-  sudo git clone https://github.com/irontiga/qortal-ui && sudo git clone https://github.com/frag-crypto/frag-default-plugins && sudo git clone https://github.com/frag-crypto/frag-core && sudo git clone https://github.com/frag-crypto/frag-qortal-crypto
-  echo -e 'EXECUTING ALL yarn install, yarn link from both frag-crypto directories and qortal-ui directory'
+  
+  sudo git clone https://github.com/QORT/qortal-ui && sudo git clone https://github.com/QORT/frag-default-plugins && sudo git clone https://github.com/QORT/frag-core && sudo git clone https://github.com/QORT/frag-qortal-crypto
+  
+  echo -e '---EXECUTING ALL PRE-BUILD TASKS---'
+  
   cd frag-core 
   sudo yarn install 
   sudo yarn unlink 
@@ -83,9 +103,13 @@ function process_full_install()
   sudo yarn link @frag-crypto/frag-core 
   sudo yarn link @frag-crypto/frag-default-plugins 
   sudo yarn link @frag-crypto/frag-qortal-crypto
+  
   echo -e '---BUILDING UI SERVER THEN RUNNING IN THIS TERMINAL WINDOW - KEEP WINDOW OPEN!---'
+  
   sudo yarn run build 
-  echo -e 'now running qortal UI server, cntrl+c to stop server, access via http://localhost' 
+  
+  echo -e '---RUNNING UI SERVER - ACCESS VIA - http://localhost:12388 - KEEP THIS WINDOW OPEN AS LONG AS YOU ARE PLANNING TO USE UI---' 
+  
   sudo yarn run server
 
 # you can use debug function
@@ -95,15 +119,21 @@ function process_full_install()
 # backup and update all git repos, yarn relink and install, and rebuild
 function process_update_all_github_and_rebuild()
 {
-  echo -e 'making backup of existing files'
+  
+  echo -e '---MAKING BACKUP OF EXISTING FOLDERS JUST IN CASE---'
+  
   sudo mkdir BACKUPS
   sudo mv frag-* BACKUPS 
   sudo mv qortal-ui BACKUPS
   sudo zip -r BACKUP-latest.zip BACKUPS
   sudo rm -R BACKUPS
-  echo -e  'cloning repos'
-  sudo git clone https://github.com/irontiga/qortal-ui && sudo git clone https://github.com/frag-crypto/frag-default-plugins && sudo git clone https://github.com/frag-crypto/frag-core && sudo git clone https://github.com/frag-crypto/frag-qortal-crypto
-  echo -e 'doing installation stuff'
+  
+  echo -e  '---CLONING NEW CODE FROM GITHUB REPOS---'
+  
+  sudo git clone https://github.com/QORT/qortal-ui && sudo git clone https://github.com/QORT/frag-default-plugins && sudo git clone https://github.com/QORT/frag-core && sudo git clone https://github.com/QORT/frag-qortal-crypto
+  
+  echo -e '---DOING ALL PRE-BUILD TASKS---'
+  
   cd frag-core
   sudo yarn install 
   sudo yarn unlink
@@ -112,12 +142,12 @@ function process_update_all_github_and_rebuild()
   sudo yarn install
   sudo yarn unlink
   sudo yarn link
-  echo -e 'fixing min.src.js file'
-  cd plugins/core
-  sudo sed -i '21s/.*/    fetch(url).then(res => console.log(res))/' main.src.js
-  echo -e 'fixed forever'
-  cd ../../../frag-qortal-crypto
-  #cd ../frag-qortal-crypto
+#  echo -e 'fixing min.src.js file'
+#  cd plugins/core
+#  sudo sed -i '21s/.*/    fetch(url).then(res => console.log(res))/' main.src.js
+#  echo -e 'fixed forever'
+#  cd ../../../frag-qortal-crypto
+  cd ../frag-qortal-crypto
   sudo yarn install
   sudo yarn unlink
   sudo yarn link
@@ -125,9 +155,13 @@ function process_update_all_github_and_rebuild()
   sudo yarn link @frag-crypto/frag-core
   sudo yarn link @frag-crypto/frag-default-plugins
   sudo yarn link @frag-crypto/frag-qortal-crypto
-  echo 'REBUILDING UI NOW'
+  
+  echo -e '---REBUILDING UI NOW---'
+  
   sudo yarn run build
-  echo 'running UI'
+ 
+  echo -e '---RUNNING UI - LEAVE THIS WINDOW OPEN TO KEEP UI RUNNING---'
+  
   sudo yarn run server 
   
   # you can use debug function
